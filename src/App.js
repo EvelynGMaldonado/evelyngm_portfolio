2a46c14cdce121d768dfbf1c74a8bc2e070a179c
Evelyn G <evelyn.gmaldonado@gmail.com>
2021-12-08 20:41:43 -0800

    working on index and app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import ContactInfo from "./components/ContactInfo/ContactInfo";
+import Footer from "./components/Footer/Footer";
+// import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume/Resume";
+// import PortfolioContainer from "./components/PortfolioContainer";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    {/* <PortfolioContainer /> */}
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element= {<Home/>} />
+        <Route exact path="/projects" element={<Projects/>}/>
+        <Route exact path="/resume" element={<Resume/>}/>
+        <Route exact path="/contact-info" element={<ContactInfo/>} />
+      </Routes>
+      <Footer/>     
+      </Router>
+      </>
   );
 }
 
