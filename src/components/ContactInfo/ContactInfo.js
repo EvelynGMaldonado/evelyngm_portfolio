1d229061855bec7bf65859119d0981104ed252e0
Evelyn G <evelyn.gmaldonado@gmail.com>
2022-02-02 11:47:42 -0800

    working on contact information

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -12,10 +12,11 @@ const ContactInfo = () => {
             <div class="col-md-8">
             <div class="card-body contact-section rounded">
                 <ul>
-                    <li><strong> Email:</strong> <a href="#">evelyn.gmaldonado@gmail.com</a></li>
-                    <li><strong>GitHub:</strong> <a href="#">EvelynGMaldoando</a></li>
-                    <li><strong> Phone number:</strong> <a href="#">206 399 7452</a></li>
-                    <li><strong>Resume:</strong> <a href="https://drive.google.com/file/d/1Sqqs20OusF5f8EVDRKvQxqN0MWf4NeXc/view?usp=sharing" >Evelyn G Maldonado Resume</a></li>
+                    <li><strong>Email:</strong> <a href="#">evelyn.gmaldonado@gmail.com</a></li>
+                    <li><strong>GitHub:</strong> <a href="https://github.com/EvelynGMaldonado">EvelynGMaldoando GitHub</a></li>
+                    <li><strong>LinkedIn:</strong> <a href=" https://www.linkedin.com/in/evelyngmaldonado/">EvelynGMaldoando LinkedIn</a></li>
+                    <li><strong>Phone number:</strong> <a href="#">(206)-399-7452</a></li>
+                    <li><strong>Resume:</strong><a href="https://drive.google.com/file/d/1uXEKBSWfHfhK_6yLZYlKPzQyRMP57X3d/view?usp=sharing">Evelyn G Maldonado Resume</a></li>
                 </ul>
             </div>
             </div>
